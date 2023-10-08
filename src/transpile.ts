5a33e8af0723c6f93508ef42a71e02ac064b6fbd
Gnlow <gnlow@protonmail.com>
2023-10-09 00:32:46 +0900

    mm

--- src/transpile.ts
+++ src/transpile.ts
@@ -1,11 +1,11 @@
-import { transpile } from "https://deno.land/x/emit@0.28.0/mod.ts"
+import { transpile } from "https://deno.land/x/emit@0.29.0/mod.ts"
 
-const url = new URL("../temp/1.ts", import.meta.url)
+const url = new URL("https://deno.land/x/enz@0.1.1/mod.ts", import.meta.url)
 const result = await transpile(url)
 
 console.log(result)
 
 await Deno.writeTextFile(
-    "dist/1.js",
+    "dist/enz.js",
     result.get(url.href) || ""
 )
